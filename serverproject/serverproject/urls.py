6635def084bad34ca185eb10d0258c9269b823ce
Antti Möttönen <antti.moettoenen@gmail.com>
2016-04-13 13:37:54 +0300

    Add deadline and estimated completion date to task

--- serverproject/serverproject/urls.py
+++ serverproject/serverproject/urls.py
@@ -39,8 +39,8 @@ class IsInTaskGroupFilter(filters.BaseFilterBackend):
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('title', 'creator', 'responsible_member', 'id', 'description')
-        read_only_fields = ('creator', 'group', 'id')
+        fields = ('title', 'creator', 'responsible_member', 'id', 'description', 'deadline', 'estimated_completion_time')
+        read_only_fields = ('creator', 'id')
 
     def create(self, validated_data):
         request = self.context['request']
